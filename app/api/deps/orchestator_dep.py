342b362d34824f179f2933b67c2516a35badde27
Andres <federicoescobar2022@gmail.com>
2026-05-11 17:16:17 -0500

    refactor: pipeline refactored. Now it is resized -> grayscale -> denoised -> eliminate_shadows -> sharpen -> deskew

--- app/api/deps/orchestator_dep.py
+++ app/api/deps/orchestator_dep.py
@@ -1,11 +1,11 @@
 from fastapi import Depends
 
 from app.api.deps.image_processing_dep import get_image_processor
-from app.modules.image_processing.preprocessing import ImagePrepocessor
+from app.modules.image_processing.preprocessing import ImagePreprocessor
 from app.orchestator.orchestator import InvoiceOrchestator
 
 
 def get_orchestator(
-    image_processor: ImagePrepocessor = Depends(get_image_processor)
+    image_processor: ImagePreprocessor = Depends(get_image_processor)
     ) -> InvoiceOrchestator:
     return InvoiceOrchestator(image_processor)
