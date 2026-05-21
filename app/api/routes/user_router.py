c6f638e405a9c6b827c86f9246e6cd502e479e90
Andres <federicoescobar2022@gmail.com>
2026-05-20 21:17:04 -0500

    refactor(authentication): user router refactored. get_by_email and by_id eliminated. Implemented GET /me, PUT /me, DELETE /me

--- app/api/routes/user_router.py
+++ app/api/routes/user_router.py
@@ -1,9 +1,8 @@
-
-from uuid import UUID
-
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.api.deps.user_service_dep import get_user_service
+from app.auth.dependencies import get_current_user
+from app.models.user import User
 from app.schemas.requests.user_create import UserCreate
 from app.schemas.requests.user_update import UserUpdate
 from app.schemas.responses.user_read import UserRead
@@ -20,34 +19,28 @@ def create_user(
     return service.create_user(userDto)
 
 
-@user_router.get("/{id_user}", response_model=UserRead)
-def get_user_by_id(
-    id_user: UUID,
-    service: UserServiceInterface = Depends(get_user_service)
+@user_router.get("/me", response_model=UserRead)
+def get_profile(
+    service: UserServiceInterface = Depends(get_user_service),
+    current_user: User = Depends(get_current_user)
 ):
-    return service.get_user_by_id(id_user)
+    
+    return service.get_user_by_id(current_user.id)
 
 
-@user_router.get("/by-email/{email}", response_model=UserRead)
-def get_by_email(
-    email: str,
-    service: UserServiceInterface = Depends(get_user_service)
-):
-    return service.get_by_email(email)
 
-
-@user_router.put("/{id_user}", response_model=UserRead)
+@user_router.put("/me", response_model=UserRead)
 def update_user(
-    id_user: UUID,
     userDto: UserUpdate,
-    service: UserServiceInterface = Depends(get_user_service)
+    service: UserServiceInterface = Depends(get_user_service),
+    current_user: User = Depends(get_current_user)
 ):
-    return service.update_user(id_user, userDto)
+    return service.update_user(current_user.id, userDto)
 
 
-@user_router.delete("/{id_user}")
+@user_router.delete("/me")
 def delete_user(
-    id_user: UUID,
-    service: UserServiceInterface = Depends(get_user_service)
+    service: UserServiceInterface = Depends(get_user_service),
+    current_user: User = Depends(get_current_user)
 ):
-    return service.delete_user(id_user)
+    return service.delete_user(current_user.id)
